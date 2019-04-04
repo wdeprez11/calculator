5bc63fef6b78dc43056cdbdf568fb4e3842f6ad1
wdeprez11 <w.deprez11@gmail.com>
2019-04-04 06:54:47 -0500

    Started work on operator method and flushing to a file, and dates

--- log.cpp
+++ log.cpp
@@ -1,37 +1,59 @@
 #include "log.h"
+#include <ctime>
+#include <fstream>
 
 Message::Message()
-    : message(" ")
+    : message("")
 {
 }
 
-Message::Message(std::string& newMessage)
+Message::Message(const std::string& newMessage)
     : message(newMessage)
 {
 }
 
-Message::Message(Message& newMessage)
+Message::Message(const Message& newMessage)
     : message(newMessage.message)
 {
 }
 
 Log::Log()
     : msgLog()
+    , startTime(ctime(time(0)))
 {
-    msgLog.reserve(10);
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    startTime = std::string(dt);
+    msgLog.reserve(100);
+    msgLog.emplace_back(Message("Initializing Log"));
+    msgLog.emplace_back(Message("Log Started at: " + startTime /*time*/));
+    msgLog.emplace_back(Message("Waiting for user..."));
 }
 
 Log::Log(const Log& oldLog)
     : msgLog(oldLog.msgLog)
     , prevMsg(oldLog.prevMsg)
+    , startTime(oldLog.startTime)
 {
-    msgLog.reserve(10);
 }
 
 Log::~Log()
 {
-    // add line
+    // add line denoting end of log
     // flush log to file
+}
+
+void Log::FlushLog()
+{
+    std::ofstream logFile;
+    logFile.open("log" + startTime);
+    for (long unsigned int i = 0; i < msgLog.size(); ++i) {
+        logFile << msgLog[i];
+    }
+    // want to call this automatically each time vector reaches capacity (every 100 lines)
+    // write msgLog vector to file
+    // clear msgLog
+    ClearLog();
 }
 
 std::string Log::GetPrevMsg() const
@@ -42,6 +64,11 @@ std::string Log::GetPrevMsg() const
 std::string Log::GetLog() const
 {
     return "";
+}
+
+std::string Log::GetStartTime() const
+{
+    return startTime;
 }
 
 void Log::DeletePrevMsg()
@@ -61,4 +88,10 @@ void Log::AddMsg(std::string& msg)
 void Log::ClearLog()
 {
     msgLog.clear();
+}
+
+std::ofstream& operator<<(std::ofstream stream, const Message& other)
+{
+    stream << other.message << '\n';
+    return stream;
 }
