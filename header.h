89110d2829210594614f1b17e39bdd8acd248663
wdeprez11 <w.deprez11@gmail.com>
2019-04-04 01:56:14 -0500

    Added initial declarations for Precise class in header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+class Precise {
+private:
+    int size;
+
+public:
+    Precise();
+    Precise(int size);
+    int GetSize() const;
+};
+
+#endif // HEADER_H
